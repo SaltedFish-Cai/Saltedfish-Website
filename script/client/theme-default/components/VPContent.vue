12e1fee34b73dc3c72a8845f33e444c33c4482b4
Cai <caiqi@bbraun.com>
2026-02-28 11:44:29 +0800

    refactor(样式): 将VPContent样式从组件移至全局样式文件
    将VPContent组件的样式定义从Vue单文件组件中移除，统一到全局的custom.scss文件中

--- script/client/theme-default/components/VPContent.vue
+++ script/client/theme-default/components/VPContent.vue
@@ -58,38 +58,4 @@ const { hasSidebar } = useSidebar();
   </div>
 </template>
 
-<style scoped>
-.VPContent {
-  flex-grow: 1;
-  flex-shrink: 0;
-  margin: var(--vp-layout-top-height, 0px) auto 0;
-  width: 100%;
-}
-
-.VPContent.is-home {
-  width: 100%;
-  max-width: 100%;
-}
-
-.VPContent.has-sidebar {
-  margin: 0;
-}
-
-@media (min-width: 960px) {
-  .VPContent {
-    padding-top: var(--vp-nav-height);
-  }
-
-  .VPContent.has-sidebar {
-    margin: var(--vp-layout-top-height, 0px) 0 0;
-    padding-left: var(--vp-sidebar-width);
-  }
-}
-
-/* @media (min-width: 1440px) {
-  .VPContent.has-sidebar {
-    padding-right: calc((100vw - var(--vp-layout-max-width)) / 2);
-    padding-left: calc((100vw - var(--vp-layout-max-width)) / 2 + var(--vp-sidebar-width));
-  }
-} */
-</style>
+<style scoped></style>
